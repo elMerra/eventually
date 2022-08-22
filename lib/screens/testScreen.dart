1f5663e345188092a0b2a0c6c72767e297285a2a
elMerra <paudm13@gmail.com>
2022-08-22 21:47:48 +0200

    SliverAppBar added and TestScreen modified for SliverAppBar

--- lib/screens/testScreen.dart
+++ lib/screens/testScreen.dart
@@ -2,18 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:pandiller_alpha/Widgets/userNameInput.dart';
 import 'package:pandiller_alpha/widgets/allButtons.dart';
 import 'package:pandiller_alpha/widgets/allCards.dart';
+import 'package:pandiller_alpha/widgets/sliverAppBar.dart';
 
 class TestScreen extends StatelessWidget {
   const TestScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: const <Widget>[],
-      ),
-    );
+    return sliverAppBar();
   }
 }
---
+++ lib/widgets/sliverAppBar.dart
@@ -0,0 +1,108 @@
+
+/*
+IMPORTANT
+SliverAppBar must be used without anything around him (ex. no columns/row or anything)
+*/
+
+
+import 'package:flutter/material.dart';
+
+class sliverAppBar extends StatefulWidget {
+  const sliverAppBar({Key? key}) : super(key: key);
+
+  @override
+  State<sliverAppBar> createState() => _sliverAppBarState();
+}
+
+class _sliverAppBarState extends State<sliverAppBar> {
+  bool _pinned = true;
+  bool _snap = false;
+  bool _floating = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //SliverAppBar is declared in Scaffold.body, in slivers of a CustomScrollView
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+            pinned: this._pinned,
+            snap: this._snap,
+            floating: this._floating,
+            expandedHeight: 160.0,
+            flexibleSpace: FlexibleSpaceBar(
+              title: const Text('FlexibleSpaceTitle'),
+              background: Image.asset(
+                'assets/images/man.png',
+                fit: BoxFit.fill,
+              ),
+            ),
+          ),
+          // If the main content is a list, use SliverList instead
+          const SliverFillRemaining(
+            child: Center(
+              child: Text('Hello'),
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: this._getBottomAppBar(),
+    );
+  }
+
+  Widget _getBottomAppBar() {
+    return BottomAppBar(
+      child: ButtonBar(
+        alignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Row(
+            children: <Widget>[
+              const Text('Pinned'),
+              Switch(
+                onChanged: (bool val) {
+                  setState(() {
+                    this._pinned = val;
+                  });
+                },
+                value: this._pinned,
+              ),
+            ],
+          ),
+          Row(
+            children: <Widget>[
+              const Text('snap'),
+              Switch(
+                onChanged: (bool val) {
+                  setState(() {
+                    this._snap = val;
+                    //Snapping only applies when the app bar is floating
+                    this._floating = this._floating || val;
+                  });
+                },
+                value: this._snap,
+              )
+            ],
+          ),
+          Row(
+            children: <Widget>[
+              const Text('Floating'),
+              Switch(
+                onChanged: (bool val) {
+                  setState(() {
+                    this._floating = val;
+                    if (this._snap == true) {
+                      if (this._floating != true) {
+                        this._snap = false;
+                      }
+                    }
+                  });
+                },
+                value: this._floating,
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
