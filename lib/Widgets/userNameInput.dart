28a83d50b0e32cc379b0d9028b266bae7d23e6ae
elMerra <paudm13@gmail.com>
2022-08-22 16:28:21 +0200

    First Widget folder creation

---
+++ lib/Widgets/userNameInput.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class UserNameInput extends StatefulWidget {
+  const UserNameInput({Key? key}) : super(key: key);
+
+  @override
+  State<UserNameInput> createState() => _UserNameInputState();
+}
+
+class _UserNameInputState extends State<UserNameInput> {
+  final GlobalKey<FormFieldState<String>> _passwordFieldKey =
+      GlobalKey<FormFieldState<String>>();
+
+  String? _name;
+  String? _phoneNumber;
+  String? _email;
+  String? _password;
+
+  String? _validateName(String? value) {
+    if (value?.isEmpty ?? false) {
+      return 'Name is required.';
+    }
+    final RegExp nameExp = RegExp(r'^[A-Za-z] + $');
+    if (!nameExp.hasMatch(value!)) {
+      return 'Please enter only alphabetical characters.';
+    }
+    return null;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          const SizedBox(height: 24.0),
+          //Name form
+          TextFormField(
+            textCapitalization: TextCapitalization.words,
+            decoration: const InputDecoration(
+              border: UnderlineInputBorder(),
+              filled: true,
+              icon: Icon(Icons.person),
+              hintText: 'What people call you?',
+              labelText: 'Name',
+            ),
+            onSaved: (String? value) {
+              this._name = value;
+              print('name=$_name');
+            },
+            validator: _validateName,
+          ),
+          const SizedBox(height: 24.0),
+          //Phone number form
+          TextFormField(
+            decoration: const InputDecoration(
+                border: UnderlineInputBorder(),
+                filled: true,
+                icon: Icon(Icons.phone),
+                hintText: 'Where can we reach you?',
+                labelText: 'Phone Number'),
+            keyboardType: TextInputType.phone,
+            onSaved: (String? value) {
+              this._phoneNumber = value;
+              print('phoneNumber=$_phoneNumber');
+            },
+            //TextInputFormatters are applied in sequence
+            inputFormatters: <TextInputFormatter>[
+              FilteringTextInputFormatter.digitsOnly
+            ],
+          ),
+          const SizedBox(height: 24.0),
+          //Email form
+          TextFormField(
+              decoration: const InputDecoration(
+                border: UnderlineInputBorder(),
+                filled: true,
+                icon: Icon(Icons.email),
+                hintText: 'Your email adress',
+                labelText: 'Email',
+              ),
+              keyboardType: TextInputType.emailAddress,
+              onSaved: (String? value) {
+                print('email$_email');
+              })
+        ],
+      ),
+    );
+  }
+}
--- lib/screens/loginscreen.dart
+++ lib/screens/loginscreen.dart
@@ -1,72 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:pandiller_alpha/Widgets/userNameInput.dart';
 
-class LoginScreen extends StatefulWidget {
+class LoginScreen extends StatelessWidget {
   const LoginScreen({Key? key}) : super(key: key);
 
   @override
-  State<LoginScreen> createState() => _LoginScreenState();
-}
-
-class _LoginScreenState extends State<LoginScreen> {
-  bool _showPassword = false;
-  // ignore: annotate_overrides
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.bottomLeft,
-            end: Alignment.topRight,
-            colors: [
-              Color.fromARGB(255, 197, 148, 74),
-              Color.fromARGB(255, 255, 247, 180)
-            ],
-          ),
-        ),
-        // ignore: avoid_unnecessary_containers
-        child: Container(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              TextField(
-                // ignore: unnecessary_this
-                obscureText: !this._showPassword,
-                decoration: InputDecoration(
-                  labelText: 'password',
-                  prefixIcon: const Icon(Icons.security),
-                  suffixIcon: IconButton(
-                    icon: Icon(
-                      Icons.remove_red_eye,
-                      color: this._showPassword ? Colors.blue : Colors.grey,
-                    ),
-                    onPressed: () {
-                      setState(() => this._showPassword = !this._showPassword);
-                    },
-                  ),
-                ),
-              ),
-              TextField(
-                // ignore: unnecessary_this
-                obscureText: !this._showPassword,
-                decoration: InputDecoration(
-                  labelText: 'password',
-                  prefixIcon: const Icon(Icons.security),
-                  suffixIcon: IconButton(
-                    icon: Icon(
-                      Icons.remove_red_eye,
-                      color: this._showPassword ? Colors.blue : Colors.grey,
-                    ),
-                    onPressed: () {
-                      setState(() => this._showPassword = !this._showPassword);
-                    },
-                  ),
-                ),
-              )
-            ],
-          ),
-        ),
-      ),
-    );
+    return Scaffold(body: Center(child: UserNameInput()));
   }
 }
