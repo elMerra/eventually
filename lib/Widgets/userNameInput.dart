cb0900eb61b7b5c9646d518a315f1e1d23a39406
elMerra <paudm13@gmail.com>
2022-08-23 16:22:23 +0200

    BottomConvexAppBar addition

--- lib/Widgets/userNameInput.dart
+++ lib/Widgets/userNameInput.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -30,81 +32,86 @@ class _UserNameInputState extends State<UserNameInput> {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      padding: const EdgeInsets.symmetric(horizontal: 16.0),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          const SizedBox(height: 24.0),
-          //Name form
-          TextFormField(
-            textCapitalization: TextCapitalization.words,
-            decoration: const InputDecoration(
-              border: UnderlineInputBorder(),
-              filled: true,
-              icon: Icon(Icons.person),
-              hintText: 'What people call you?',
-              labelText: 'Name',
-            ),
-            onSaved: (String? value) {
-              this._name = value;
-              print('name=$_name');
-            },
-            validator: _validateName,
-          ),
-          const SizedBox(height: 24.0),
-          //Password form
-          TextFormField(
-            textCapitalization: TextCapitalization.words,
-            decoration: const InputDecoration(
-              border: UnderlineInputBorder(),
-              filled: true,
-              icon: Icon(Icons.password),
-              hintText: 'Which password do you want?',
-              labelText: 'Password',
-            ),
-            onFieldSubmitted: (String? value) {
-              this._password = value;
-              print('password=$_password');
-            },
-            validator: _validateName,
-          ),
-          const SizedBox(height: 24.0),
-          //Phone number form
-          TextFormField(
-            decoration: const InputDecoration(
-                border: UnderlineInputBorder(),
-                filled: true,
-                icon: Icon(Icons.phone),
-                hintText: 'Where can we reach you?',
-                labelText: 'Phone Number'),
-            keyboardType: TextInputType.phone,
-            onSaved: (String? value) {
-              this._phoneNumber = value;
-              print('phoneNumber=$_phoneNumber');
-            },
-            //TextInputFormatters are applied in sequence
-            inputFormatters: <TextInputFormatter>[
-              FilteringTextInputFormatter.digitsOnly
+    return Center(
+      child: Container(
+        margin: EdgeInsets.only(top: 350),
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              const SizedBox(height: 24.0),
+              //Name form
+              TextFormField(
+                textCapitalization: TextCapitalization.words,
+                decoration: const InputDecoration(
+                  border: UnderlineInputBorder(),
+                  filled: true,
+                  icon: Icon(Icons.person),
+                  hintText: 'What people call you?',
+                  labelText: 'Name',
+                ),
+                onSaved: (String? value) {
+                  this._name = value;
+                  print('name=$_name');
+                },
+                validator: _validateName,
+              ),
+              const SizedBox(height: 24.0),
+              //Password form
+              TextFormField(
+                textCapitalization: TextCapitalization.words,
+                decoration: const InputDecoration(
+                  border: UnderlineInputBorder(),
+                  filled: true,
+                  icon: Icon(Icons.password),
+                  hintText: 'Which password do you want?',
+                  labelText: 'Password',
+                ),
+                onFieldSubmitted: (String? value) {
+                  this._password = value;
+                  print('password=$_password');
+                },
+                validator: _validateName,
+              ),
+              const SizedBox(height: 24.0),
+              //Phone number form
+              TextFormField(
+                decoration: const InputDecoration(
+                    border: UnderlineInputBorder(),
+                    filled: true,
+                    icon: Icon(Icons.phone),
+                    hintText: 'Where can we reach you?',
+                    labelText: 'Phone Number'),
+                keyboardType: TextInputType.phone,
+                onSaved: (String? value) {
+                  this._phoneNumber = value;
+                  print('phoneNumber=$_phoneNumber');
+                },
+                //TextInputFormatters are applied in sequence
+                inputFormatters: <TextInputFormatter>[
+                  FilteringTextInputFormatter.digitsOnly
+                ],
+              ),
+              const SizedBox(height: 24.0),
+              //Email form
+              TextFormField(
+                decoration: const InputDecoration(
+                  border: UnderlineInputBorder(),
+                  filled: true,
+                  icon: Icon(Icons.email),
+                  hintText: 'Your email adress',
+                  labelText: 'Email',
+                ),
+                keyboardType: TextInputType.emailAddress,
+                onSaved: (String? value) {
+                  print('email$_email');
+                },
+              ),
             ],
           ),
-          const SizedBox(height: 24.0),
-          //Email form
-          TextFormField(
-            decoration: const InputDecoration(
-              border: UnderlineInputBorder(),
-              filled: true,
-              icon: Icon(Icons.email),
-              hintText: 'Your email adress',
-              labelText: 'Email',
-            ),
-            keyboardType: TextInputType.emailAddress,
-            onSaved: (String? value) {
-              print('email$_email');
-            },
-          ),
-        ],
+        ),
       ),
     );
   }
--- lib/screens/testScreen.dart
+++ lib/screens/testScreen.dart
@@ -1,18 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:pandiller_alpha/Widgets/allButtons.dart';
-import 'package:pandiller_alpha/Widgets/allCards.dart';
+import 'package:pandiller_alpha/widgets/bottomConvexAppBar.dart';
 
 class TestScreen extends StatelessWidget {
   const TestScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: const [],
-      ),
+    return const SafeArea(
+      child: ConvexAppExample(),
     );
   }
 }
